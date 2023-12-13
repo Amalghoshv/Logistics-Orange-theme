95def73226c8135407f95378da5dd98973f1e7b7
Amalghoshv <amalghoshv@gmail.com>
2023-12-13 23:42:40 +0530

    first commit

--- orangetheme/hooks.py
+++ orangetheme/hooks.py
@@ -10,11 +10,11 @@ app_license = "mit"
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = "/assets/orangetheme/css/orangetheme.css"
+app_include_css = "/assets/orangetheme/css/orangewhite.css"
 # app_include_js = "/assets/orangetheme/js/orangetheme.js"
 
 # include js, css files in header of web template
-# web_include_css = "/assets/orangetheme/css/orangetheme.css"
+web_include_css = "/assets/orangetheme/css/orangewhite.css"
 # web_include_js = "/assets/orangetheme/js/orangetheme.js"
 
 # include custom scss in every website theme (without file extension ".scss")
